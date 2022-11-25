72ebcf8ec724c466f15e303d08c9be79e134b3cf
Nick Kosarev <hmbanan666@hotmail.com>
2022-11-25 17:50:56 +0200

    vite init
    Co-Authored-By: Nick Kosarev <hmbanan666@hotmail.com>
    Co-Authored-By: Sleeplessness8 <89253141+Sleeplessness8@users.noreply.github.com>

--- index.js
+++
@@ -1,2 +0,0 @@
-const test = 1;
-const test2 = 2;
---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  const test = 1;
+
+  return (
+    <div className="App">
+      <canvas
+        id="canvas"
+        width="1000"
+        height="1000"
+        style={{ background: "#fff" }}
+      ></canvas>
+    </div>
+  );
+}
+
+export default App;
