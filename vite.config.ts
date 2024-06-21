1a6438eddad719338c4037deaee3f27e310cb6fa
Alexander <me@hywax.space>
2024-06-21 21:29:37 +0300

    feat: dev containers (#174)

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,13 @@ export default defineConfig({
     sveltekit(),
     svelteTesting(),
   ],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    watch: {
+      usePolling: true,
+    },
+  },
   test: {
     globals: true,
     environment: 'happy-dom',
