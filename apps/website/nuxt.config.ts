a3346a3101a08a78cedf5da2c2ef47d4a3f9d524
Nick Kosarev <hmbanan666@hotmail.com>
2024-08-19 15:56:55 +0200

    feat: nx monorepo (#200)
    * feat: nx monorepo
    
    * chore: dev container
    
    * chore: dockerfile and ci
    
    * fix: cloud

---
+++ apps/website/nuxt.config.ts
@@ -0,0 +1,33 @@
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import { defineNuxtConfig } from 'nuxt/config';
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  workspaceDir: '../../',
+  srcDir: 'src',
+  devtools: { enabled: true },
+
+  devServer: {
+    host: '0.0.0.0',
+    port: 4200,
+  },
+
+  typescript: {
+    typeCheck: true,
+    tsConfig: {
+      extends: '../tsconfig.app.json', // Nuxt copies this string as-is to the `./.nuxt/tsconfig.json`, therefore it needs to be relative to that directory
+    },
+  },
+
+  imports: {
+    autoImport: true,
+  },
+
+  css: ['~/assets/css/styles.css'],
+
+  vite: {
+    plugins: [nxViteTsPaths()],
+  },
+
+  compatibilityDate: '2024-08-18',
+});
