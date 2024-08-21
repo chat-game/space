5d7f081e0d3bf0a881176acb7a851273ca765055
Nick Kosarev <hmbanan666@hotmail.com>
2024-08-21 17:20:07 +0200

    chore: cookie tests (#207)

---
+++ apps/website/src/server/api/auth/me.get.ts
@@ -0,0 +1,17 @@
+export default defineEventHandler((event) => {
+  const { public: publicEnv } = useRuntimeConfig();
+  
+  const jwt = getCookie(event, publicEnv.cookieKey)
+  if (!jwt) {
+    throw createError({
+      statusCode: 401,
+      statusMessage: 'Unauthorized',
+    })
+  }
+
+  return {
+    id: '1234',
+    twitchId: '12345',
+    userName: 'testuser',
+  }
+});
