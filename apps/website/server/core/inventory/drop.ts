1d333e9dc4f176cbea2f4f26b491c1e8f1861f36
Nick Kosarev <hmbanan666@hotmail.com>
2024-12-24 20:12:17 +0200

    fix: prisma (#332)

---
+++ apps/website/server/core/inventory/drop.ts
@@ -0,0 +1,66 @@
+import { createId } from '@paralleldrive/cuid2'
+
+export async function dropChristmasCupcake(telegramId: string, type: 'TREE') {
+  const itemId = 'k3bitdush5wqbwphhdfnxqtl'
+  const leaderboardId = 'iq9f2634d3q3ans243dhxmj7'
+
+  // tree: 20% chance
+  if (type === 'TREE' && Math.random() < 0.2) {
+    const telegramProfile = await prisma.telegramProfile.findFirst({
+      where: { telegramId },
+      include: {
+        profile: {
+          include: {
+            itemEditions: true,
+            leaderboardMembers: true,
+          },
+        },
+      },
+    })
+    if (telegramProfile && telegramProfile?.profile) {
+      // add item or +1 to amount
+      const item = telegramProfile.profile.itemEditions.find((item) => item.id === itemId)
+      if (item) {
+        await prisma.inventoryItemEdition.update({
+          where: { id: item?.id },
+          data: {
+            amount: {
+              increment: 1,
+            },
+          },
+        })
+      } else {
+        await prisma.inventoryItemEdition.create({
+          data: {
+            id: createId(),
+            itemId,
+            profileId: telegramProfile.profile.id,
+            amount: 1,
+          },
+        })
+      }
+
+      // add +1 to leaderboard member
+      const member = telegramProfile.profile.leaderboardMembers.find((member) => member.leaderboardId === leaderboardId)
+      if (member) {
+        await prisma.leaderboardMember.update({
+          where: { id: member?.id },
+          data: {
+            points: {
+              increment: 1,
+            },
+          },
+        })
+      } else {
+        await prisma.leaderboardMember.create({
+          data: {
+            id: createId(),
+            leaderboardId,
+            profileId: telegramProfile.profile.id,
+            points: 1,
+          },
+        })
+      }
+    }
+  }
+}
