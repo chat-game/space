5c566b3383d56c93492c3c0d9832f9d3f82d0277
Nick Kosarev <hmbanan666@hotmail.com>
2024-12-30 13:58:44 +0000

    feat: redis for rooms data

--- apps/website/server/plugins/04.startWagonRoom.ts
+++ apps/website/server/plugins/04.startWagonRoom.ts
@@ -3,12 +3,12 @@ import { WagonRoom } from '../core/rooms/wagon'
 
 const wagonRoomId = '12345'
 
-export default defineNitroPlugin(() => {
+export default defineNitroPlugin(async () => {
   const logger = useLogger('plugin-start-wagon-room')
 
   if (!activeRooms.find((room) => room.id === wagonRoomId)) {
     activeRooms.push(new WagonRoom({ id: wagonRoomId, token: wagonRoomId }))
   }
 
-  logger.log('Wagon room created')
+  logger.success('Wagon room created')
 })
