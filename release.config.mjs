1529944ee82eb5f84474c60c12358e8c244d094f
Nick Kosarev <hmbanan666@hotmail.com>
2025-01-06 15:37:18 +0000

    fix: deploy

--- release.config.mjs
+++ release.config.mjs
@@ -9,30 +9,27 @@ export default {
           { breaking: true, release: 'major' },
           { type: 'feat', release: 'minor' },
           { type: 'fix', release: 'patch' },
-          { type: 'chore', release: false },
+          { type: 'chore', release: 'patch' },
+          { type: 'refactor', release: 'patch' },
         ],
       },
     ],
     '@semantic-release/release-notes-generator',
     [
-      '@semantic-release/changelog',
-      {
-        changelogFile: 'CHANGELOG.md',
-      },
-    ],
-    [
       '@semantic-release/exec',
       {
         // eslint-disable-next-line no-template-curly-in-string
-        prepareCmd: 'pnpm version ${nextRelease.version} --git-tag-version=false',
-      },
-    ],
-    [
-      '@semantic-release/git',
-      {
-        assets: ['CHANGELOG.md', 'package.json', 'apps/**/CHANGELOG.md', 'apps/**/package.json'],
+        publishCmd: 'pnpm version ${nextRelease.version} --git-tag-version=false',
       },
     ],
     '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'apps/**/package.json'],
+        // eslint-disable-next-line no-template-curly-in-string
+        message: 'chore(release): ${nextRelease.version} [skip ci]',
+      },
+    ],
   ],
 }
