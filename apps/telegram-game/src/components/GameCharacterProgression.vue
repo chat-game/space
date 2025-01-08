b8e20294aace4b4c91f2ad09b31780a807a8a690
Nick Kosarev <hmbanan666@hotmail.com>
2025-01-08 21:55:19 +0200

    chore: some idea (#400)

--- apps/telegram-game/src/components/GameCharacterProgression.vue
+++ apps/telegram-game/src/components/GameCharacterProgression.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-row gap-2">
     <div class="z-10 relative w-16 h-16 flex flex-col justify-center items-center bg-gradient-to-br from-orange-300 to-orange-500 rounded-2xl">
       <Image :src="character?.character.codename ? `units/${character?.character.codename}/head.png` : 'units/telegramo/head.png'" class="p-1.5 h-16 w-16" />
-      <div class="absolute -bottom-1 -right-3 px-1 w-fit min-w-6 bg-orange-50 text-orange-600 text-base font-semibold text-center rounded-full">
+      <div class="absolute -bottom-2 -right-3 px-1 w-fit min-w-6 bg-orange-50 text-orange-600 text-base font-semibold text-center rounded-full">
         {{ character?.level ?? 1 }}
       </div>
     </div>
