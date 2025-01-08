a921d98b4b83fac00c15709969ed221c0a696e83
Nick Kosarev <hmbanan666@hotmail.com>
2025-01-08 19:39:49 +0200

    feat: updates (#398)
    * feat: updates
    
    * chore: created event

--- apps/telegram-game/src/utils/gameClient.ts
+++ apps/telegram-game/src/utils/gameClient.ts
@@ -2,8 +2,12 @@ import { BaseGameAddon } from '@chat-game/game'
 
 const { refreshCharacter } = useCharacter()
 
+function updateUI() {
+  refreshCharacter()
+}
+
 const websocketUrl = 'wss://chatgame.space/api/websocket'
-const gameClient = new BaseGameAddon({ websocketUrl, client: 'TELEGRAM_CLIENT', updateUI: refreshCharacter })
+const gameClient = new BaseGameAddon({ websocketUrl, client: 'TELEGRAM_CLIENT', updateUI })
 
 const roomConnected = ref<string>()
 
