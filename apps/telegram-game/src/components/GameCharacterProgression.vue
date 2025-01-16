fa7644f28b06e9fbfcd4c70d52062b707735a5ae
Nick Kosarev <hmbanan666@hotmail.com>
2025-01-16 16:18:38 +0200

    fix: xp bar (#422)

--- apps/telegram-game/src/components/GameCharacterProgression.vue
+++ apps/telegram-game/src/components/GameCharacterProgression.vue
@@ -7,8 +7,8 @@
       </div>
     </div>
 
-    <div class="relative h-8 grow bg-blue-900 rounded-2xl">
-      <div class="h-full bg-gradient-to-br from-blue-300 to-blue-600 rounded-2xl duration-1000" :style="{ width: `${character?.xp && character.nextLevel && character.currentLevel ? ((character.xp - character.currentLevel.requiredXp) / (character.nextLevel.requiredXp - character.currentLevel.requiredXp)) * 100 : 100}%` }" />
+    <div class="relative h-8 grow bg-blue-900 rounded-2xl overflow-hidden">
+      <div class="h-full bg-gradient-to-br from-blue-300 to-blue-600 rounded-r-xl duration-1000" :style="{ width: `${progressWidth}%` }" />
       <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-row justify-center items-center">
         <p v-if="character?.nextLevel" class="font-semibold text-lg text-white tracking-tight">
           {{ character?.xp ?? 0 }} <span class="text-xs">XP</span>
@@ -23,4 +23,6 @@
 
 <script setup lang="ts">
 const { character } = useCharacter()
+
+const progressWidth = computed(() => character.value?.xp && character.value.nextLevel && character.value.currentLevel ? ((character.value.xp - character.value.currentLevel.requiredXp) / (character.value.nextLevel.requiredXp - character.value.currentLevel.requiredXp)) * 100 : 100)
 </script>
