7786a3108fe2420dc0e9cabad51f3778a01eefb6
Nick Kosarev <hmbanan666@hotmail.com>
2025-01-17 17:45:03 +0000

    fix: inset

--- apps/telegram-game/src/components/PageContainer.vue
+++ apps/telegram-game/src/components/PageContainer.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="tg-secondary-bg tg-text tg-content-safe-area overflow-hidden z-30 h-full min-h-dvh w-full">
-    <div class="px-4 py-2 max-w-[28rem] mx-auto flex flex-col gap-y-6">
-      <slot />
+  <div class="tg-secondary-bg tg-text tg-safe-area overflow-hidden z-30 h-full min-h-dvh w-full">
+    <div class="tg-content-safe-area">
+      <div class="px-4 py-2 max-w-[28rem] mx-auto flex flex-col gap-y-6">
+        <slot />
+      </div>
     </div>
   </div>
 </template>
