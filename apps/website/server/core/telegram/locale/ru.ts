f0bcf13b9e51283dc00fb668d9904d991983f83e
Nick Kosarev <hmbanan666@hotmail.com>
2025-01-22 12:58:08 +0200

    chore: bot rework (#452)
    * chore: bot rework
    
    * fix: import
    
    * fix: import

---
+++ apps/website/server/core/telegram/locale/ru.ts
@@ -0,0 +1,23 @@
+import type { Dictionary } from '.'
+
+export const ru: Dictionary = {
+  chatgame: {
+    welcomeMessage: `Добро пожаловать в ChatGame! 🥳
+
+Выбери игру или действие 👇`,
+    playingOnTwitch: '👾 Играем на Twitch',
+  },
+  woodland: {
+    welcomeMessage: `Добро пожаловать в Woodlands! 🥳
+
+Одна из задач - сопровождать Машину из точки А в точку Б. По пути могут встречаться препятствия. Тапай их! 👆💪
+
+Участвуй в событиях, приглашай друзей, добывай Монеты и разблокируй вручную созданных персонажей. 🤴🎅🐶`,
+    title: '🌲 Woodlands: Онлайн-игра',
+    play: '🎮 Играть',
+    developingGameOnTwitch: '👾 Улучшаем игру на Twitch',
+    website: '👨‍💻 Веб-сайт проекта',
+  },
+  subscribeToChannel: '📢 Подпишись на канал',
+  defaultBotReply: 'Я пока не умею отвечать на сообщения. Свяжись с @hmbanan666, если есть вопросы.',
+}
