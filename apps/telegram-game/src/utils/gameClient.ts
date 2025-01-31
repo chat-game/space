60e897d13e2eae7d994340a9303ad9eb447b7f30
Nick Kosarev <hmbanan666@hotmail.com>
2025-01-31 14:42:44 +0200

    fix: nginx rights (#474)
    * fix: nginx writes
    
    * fix: chown

--- apps/telegram-game/src/utils/gameClient.ts
+++ apps/telegram-game/src/utils/gameClient.ts
@@ -3,13 +3,17 @@ import { BaseGameAddon } from '@chat-game/game'
 const websocketUrl = 'wss://chatgame.space/api/websocket'
 const gameClient = new BaseGameAddon({ websocketUrl, client: 'TELEGRAM_CLIENT' })
 
-const roomConnected = ref<string>()
+const roomConnected = ref<string | null>(null)
 const isLoading = ref(false)
 
 function setAsLoaded() {
-  setTimeout(() => {
-    isLoading.value = false
-  }, 1500)
+  try {
+    setTimeout(() => {
+      isLoading.value = false
+    }, 1500)
+  } catch (error) {
+    console.error('Error in setAsLoaded:', error)
+  }
 }
 
 export { gameClient, isLoading, roomConnected, setAsLoaded }
